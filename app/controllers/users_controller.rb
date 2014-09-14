f881bd57a8f2549bcb5f45e2996e862c1301cb59
zagoranov <pijammer@gmail.com>
2014-09-14 19:17:34 +0400

    Friendship buttons, Sidebar news

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,23 @@ def show
 end
 
 
+def edit
+  @user = current_user
+end
+
+
+
+def update
+  @user = User.find(params[:id])
+ 
+  if @user.update(user_params)
+    redirect_to @user
+  else
+    render 'edit'
+  end
+end
+
+
 private
 def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
