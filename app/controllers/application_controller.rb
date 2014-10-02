1907baaa11015e8a3a5b94c10782bd0cfad38d2a
zagoranov <pijammer@gmail.com>
2014-10-02 15:22:23 +0400

    locale changing 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-#  before_action :set_locale
+  before_action :set_locale
 
 
   def set_locale
 #    I18n.locale = params[:locale] || I18n.default_locale
+   if !I18n.locale 
      if extract_locale_from_accept_language_header == "ru"
      	I18n.locale = :ru
      else
      	I18n.locale = :en
      end
+    end
   end
 
 
