67d816191d40f6be9b28158eb9b432972cc5ec7e
zagoranov <pijammer@gmail.com>
2014-11-14 23:46:43 +0300

    SQL stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -35,11 +35,11 @@ end
 
 
 def load  #sql loading stuff
- if current_user && current_user.admin
-   CONN.execute "Update trainings set day_id = (select max(id) from days) where day_id is null"
-   #CONN.execute "INSERT INTO dictitems ..."
-   redirect_to root_path, :notice => t(:data_added)
- end
+  if current_user && current_user.admin
+    CONN.execute "Update trainings set day_id = 544 where day_id is null"
+    #CONN.execute "INSERT INTO dictitems ..."
+    redirect_to root_path, :notice => t(:data_added)
+  end
 end
 
 
