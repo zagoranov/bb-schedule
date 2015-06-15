f6379e11b08da902f90746660145769521aea1e4
zagoranov <pijammer@gmail.com>
2015-06-15 19:22:54 +0300

    mail me on new reg3

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "announcer@evenlift.ru"
+  default from: "announcer@inter-activity.ru"
 
 #  def request_email(user, game, dt)   # Нотификатор Админу про новый запрос
   def newreg_email(user)   # Нотификатор Админу про новый запрос
